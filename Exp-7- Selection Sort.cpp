ecf1a6af9103b1a0f3100c5920f18be78bfe6503
shaik jigunu(192111295) <113428415+jigunu-123@users.noreply.github.com>
2024-06-12 22:03:11 +0530

    Add files via upload

---
+++ Exp-7- Selection Sort.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,max,min,n,j;
+	printf("shaik jigunu-192111295\n");
+	printf("Enter array size:");
+	scanf("%d",&n);
+	printf("Enter array elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[min]>a[j])
+				min=j;
+		}
+		if(min!=i)
+		{
+			max=a[i];
+			a[i]=a[min];
+			a[min]=max;
+		}
+	}
+	printf("Sorted Elements \n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	return 0;
+}
