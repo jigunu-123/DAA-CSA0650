f1d92837c7f61b34327a0612a07b9f7ef74094fe
shaik jigunu(192111295) <113428415+jigunu-123@users.noreply.github.com>
2024-06-12 21:51:28 +0530

    Add files via upload

---
+++ Exp-3-GCD.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,gcd,i;
+	printf("shaik jigunu-192111295\n");
+	printf("Enter first number : ");
+	scanf("%d",&a);
+	printf("Enter second number : ");
+	scanf("%d",&b);
+	for(i=1;i<=a && i<=b;++i)
+	{
+		if(a%i==0 && b%i==0)
+			gcd=i;
+	}
+	printf("GCD of given two numbers is : %d",gcd);
+}
