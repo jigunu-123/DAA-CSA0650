4e07a4e1a2c9063c620d68ffe08ee700c8024ca3
shaik jigunu(192111295) <113428415+jigunu-123@users.noreply.github.com>
2024-06-12 21:29:28 +0530

    Add files via upload

---
+++ Exp-6-Prime Number.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,flag=0;
+	printf("Enter any Number : ");
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+			flag=1;
+	}
+	if(flag==0)
+		printf("%d is a Prime Number ",n);
+	else
+		printf("%d is Not a Prime Number ",n);
+	return 0;
+}
