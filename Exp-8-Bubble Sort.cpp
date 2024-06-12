7a26b98e55cf852cbb3a81966456b84a1cd4a55f
shaik jigunu(192111295) <113428415+jigunu-123@users.noreply.github.com>
2024-06-12 22:03:37 +0530

    Add files via upload

---
+++ Exp-8-Bubble Sort.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,j,n,x;
+	printf("shaik jigunu-192111295\n");
+	printf("Enter array size: ");
+	scanf("%d",&n);
+	printf("Enter array Elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<(n-1);i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				x=a[i];
+				a[i]=a[j];
+				a[j]=x;
+			}
+		}
+	}
+	printf("Sorted Array\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+}
